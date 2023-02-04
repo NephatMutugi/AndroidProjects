69e4125da34eea69bc0a45728fd3cf0540c7f230
KEN20960 <muchirinephat5@gmail.com>
2023-02-04 15:32:27 +0200

    I'm too lazy to insert a comment

--- QuizApp/app/src/main/java/com/neph/quizapp/QuizQuestionsActivity.kt
+++ QuizApp/app/src/main/java/com/neph/quizapp/QuizQuestionsActivity.kt
@@ -2,10 +2,17 @@ package com.neph.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.neph.quizapp.models.Constants
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        val questionsList = Constants.getQuestions()
+        Log.i("QuestionsList Size: ", "${questionsList.size}")
     }
+
 }
