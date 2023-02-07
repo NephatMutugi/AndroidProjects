50ee552b7b878931cff4858705a3d557adcfcf51
KEN20960 <muchirinephat5@gmail.com>
2023-02-07 12:13:35 +0200

    Fun with flags spring crud

---
+++ backend/fun_with_flags/neph-funwithflags-crud/src/main/java/com/neph/main/FunWithFlagsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.neph.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunWithFlagsCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunWithFlagsCrudApplication.class, args);
+    }
+
+}
