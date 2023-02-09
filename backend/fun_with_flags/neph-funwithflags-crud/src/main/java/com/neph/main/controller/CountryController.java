3537cb32f1d1bacf2dd20e78bc0be4bf1ee09e6b
KEN20960 <muchirinephat5@gmail.com>
2023-02-09 20:44:07 +0200

    Countries services

--- backend/fun_with_flags/neph-funwithflags-crud/src/main/java/com/neph/main/controller/CountryController.java
+++ backend/fun_with_flags/neph-funwithflags-crud/src/main/java/com/neph/main/controller/CountryController.java
@@ -1,12 +1,45 @@
 package com.neph.main.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.neph.main.entity.Country;
+import com.neph.main.model.RequestPayload;
+import com.neph.main.model.ResponsePayload;
+import com.neph.main.service.CountryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @ Author NMuchiri
  **/
-@RequestMapping
+@RequestMapping("api/v1/country")
 @RestController
 public class CountryController {
+
+    private final CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping(value = "/all", produces = {"application/json"})
+    public ResponseEntity<List<Country>> findAllCountries(){
+        return countryService.findAllCountries();
+    }
+
+    @PostMapping(value = "/continent", produces = {"application/json"}, consumes = {"application/json"})
+    public ResponseEntity<List<Country>> findAllInContinent(@RequestBody RequestPayload requestPayload){
+        return countryService.findAllInContinent(requestPayload);
+    }
+
+    @PostMapping(value = "one", produces = {"application/json"}, consumes = {"application/json"})
+    public ResponseEntity<?> findCountry(@RequestBody RequestPayload requestPayload){
+        return countryService.findCountry(requestPayload);
+    }
+
+    @PostMapping(value = "save", produces = {"application/json"}, consumes = {"application/json"})
+    public ResponseEntity<ResponsePayload> addCountry(@RequestBody RequestPayload requestPayload){
+        return countryService.save(requestPayload);
+    }
+
 }
