d69ef3ba0d60e2fbb27aa948642587975abb81bb
KEN20960 <muchirinephat5@gmail.com>
2023-02-13 20:20:15 +0200

    Flags repo

--- backend/fun_with_flags/neph-funwithflags-crud/src/main/java/com/neph/main/repo/FlagRepository.java
+++ backend/fun_with_flags/neph-funwithflags-crud/src/main/java/com/neph/main/repo/FlagRepository.java
@@ -2,9 +2,18 @@ package com.neph.main.repo;
 
 import com.neph.main.entity.Flag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * @ Author NMuchiri
  **/
 public interface FlagRepository extends JpaRepository<Flag, Integer> {
+
+    Flag findFlagByCountryCountryName(String countryName);
+
+    @Query("SELECT F FROM Flag F INNER JOIN F.country C INNER JOIN C.continent C2 WHERE C2.continentName=:continentName")
+    List<Flag> findFlagsByContinent(@Param("continentName") String continentName);
 }
